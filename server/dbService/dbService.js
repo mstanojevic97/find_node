a56fb4c43f36749cb6ddbc80583155ca88b60808
nikolailic161 <nikola.ilic774.ni@gmail.com>
2022-02-15 20:36:56 +0100

    Home router add

--- server/dbService/dbService.js
+++ server/dbService/dbService.js
@@ -1,9 +1,9 @@
 const request=require("./request");
 
-async function getLoad()
+async function getLoad(load)
 {
-    let sql="SELECT * FROM load"
-    return request.query(sql,[]);
+    let sql="SELECT * FROM find.load WHERE loadType=?"
+    return request.query(sql,[load]);
 }
 async function getRole()
 {
@@ -35,10 +35,10 @@ async function deleteCompany(id)
     let sql="DELETE FROM company WHERE idCompany=?"
     return request.query(sql,[id]);
 }
-async function updateCompany(idCompany,companyName,VAT,email,password)
+async function updateCompany(idCompany,companyName,VAT,email)
 {
-    let sql="UPDATE company SET companyName=?,VAT=?,email=?,password=? WHERE idCompany=?"
-    return request.query(sql,[companyName,VAT,email,password,idCompany]);
+    let sql="UPDATE company SET companyName=?,VAT=?,email=? WHERE idCompany=?"
+    return request.query(sql,[companyName,VAT,email,idCompany]);
 }
 async function getAdminId(id)
 {
@@ -70,13 +70,13 @@ async function sendFreight(weight,length,warehouse,destination,note,price,idProd
     let sql="INSERT INTO freight (weight,length,warehouse,destination,note,price,idProducer,idSupplier,idStatus,idLoad) VALUES(?,?,?,?,?,?,?,NULL,1,?)"
     return request.query(sql,[weight,length,warehouse,destination,note,price,idProducer,idLoad]);
 }
-async function getFreight()
+async function getFreightTaken()
 {
-    let sql="SELECT * FROM freight"
+    let sql="SELECT freight.idFreight,freight.weight,freight.length,freight.warehouse,freight.destination,freight.note,freight.price,find.company.companyName,find.company.VAT,find.company.email,find.company.companyName,find.company.VAT,find.company.email,status.statusName,load.loadType FROM((((freight INNER JOIN status ON freight.idStatus=status.idStatus) INNER JOIN find.load ON freight.idLoad=find.load.idLoad) INNER JOIN find.company ON freight.idSupplier=find.company.idCompany) INNER JOIN find.company ON freight.idProducer=find.company.idCompany) WHERE freight.idSupplier IS NOT NULL;"
     return request.query(sql,[]);
 }
 async function getFreightProducerTaken(id)
-{
+{       
     let sql="SELECT freight.weight,freight.length,freight.warehouse,freight.destination,freight.note,freight.price,find.company.companyName,find.company.VAT,find.company.email,status.statusName,find.load.loadType FROM (((freight INNER JOIN status ON freight.idStatus=status.idStatus) INNER JOIN find.load ON freight.idLoad=find.load.idLoad)INNER JOIN find.company ON freight.idSupplier=find.company.idCompany) WHERE idProducer=?;"
     return request.query(sql,[id]);
 }
@@ -103,8 +103,15 @@ async function takeFreight(idSuplier,idFreight)
 async function updateFreight(weight,length,warehouse,destination,note,price,idLoad,id)
 {
     let sql="UPDATE find.freight SET freight.weight=?,freight.length=?,freight.warehouse=?,freight.destination=?,freight.note=?,freight.price=?,freight.idLoad=? WHERE idFreight=?"
-    return request.query(sql,[weight,length,warehouse,destination,note,price,idLoad,id])
+    return request.query(sql,[weight,length,warehouse,destination,note,price,idLoad,id]);
 }
+
+async function getFreightId (id)
+{
+    let sql ="SELECT freight.weight,freight.length,freight.warehouse,freight.destination,freight.note,freight.price,load.loadType FROM (freight INNER JOIN find.load ON freight.idLoad=load.idLoad) WHERE idFreight=?"
+    return request.query(sql,[id]);
+}
+
 
 exports.getLoad=getLoad;
 exports.getRole=getRole;
@@ -120,10 +127,11 @@ exports.sendAdmin=sendAdmin;
 exports.updateAdmin=updateAdmin;
 exports.deleteAdmin=deleteAdmin;
 exports.sendFreight=sendFreight;
-exports.getFreight=getFreight;
+exports.getFreightTaken=getFreightTaken;
 exports.getFreightProducerFree=getFreightProducerFree;
 exports.getFreightProducerTaken=getFreightProducerTaken;
 exports.getFreightSupplierFree=getFreightSupplierFree;
 exports.getFreightSupplierTaken=getFreightSupplierTaken;
 exports.takeFreight=takeFreight;
 exports.updateFreight=updateFreight;
+exports.getFreightId=getFreightId;
--- server/routs/home.js
+++ server/routs/home.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const router = express.Router();
-const {getCompanyId,getFreightProducerTaken,getFreightProducerFree} =require("../dbService/dbService");
+const {getCompanyId,getFreightProducerTaken,getFreightProducerFree,getFreightSupplierFree,getFreightSupplierTaken,getFreightTaken,getAdminEmail,updateCompany, getLoad, sendFreight, getFreightId, updateFreight} =require("../dbService/dbService");
 
 router.get("/producer",async(req,res)=>{
     const id = req.body.id;// id se vadi iz jwt tokena :D
@@ -17,7 +17,102 @@ router.get("/producer",async(req,res)=>{
         "freightFree":freightFree,
         "freightTaken":freightTaken
     });
-});
+})
+    router.get("/supplier",async(req,res)=>{
+        const id= req.body.id;
+        const supplier=await getCompanyId(id);
+        const freightTaken=await getFreightSupplierTaken(id);
+        const freightFree=await getFreightSupplierFree(id);
+        const supplierData={
+            companyName:supplier[0].companyName,
+            VAT:supplier[0].VAT,
+            email:supplier[0].email
+        }
+        res.json({
+            "supplier":supplierData,
+            "freightFree":freightFree,
+            "freightTaken":freightTaken
+        });
+    });
+    router.get("/admin",async(req,res)=>{
+        const email = req.body.email;
+        const admin=await getAdminEmail(email);
+        const adminData={
+            adminName:admin[0].name,
+            surname:admin[0].surname,
+            email:admin[0].email    
+        }
+        res.json({
+            "admin":adminData            
+        });
+    })
+    router.post("/producer/update",async(req,res)=>{
+        const id = req.body.id; // iz jwt
+        const updateData ={
+            companyName:req.body.companyName,
+            VAT:req.body.VAT,
+            email:req.body.email
+        }
+        const report = await updateCompany (id,updateData.companyName,updateData.VAT,updateData.email);
+        res.send("Uspesna izmena");
+    })
+        router.post('/producer/add',async(req,res)=>{
+        const id = req.body.id; // jwt
+        const load = req.body.load;
+        const loadType = await getLoad(load);
+        const data={
+            weight:req.body.weight,
+            length:req.body.length,
+            warehouse:req.body.warehouse,
+            destination:req.body.destination,
+            note:req.body.note,
+            price:req.body.price,
+            idProducer:id,
+            idLoad:loadType[0].idLoad
+        }
+        const report =await sendFreight (data.weight,data.length,data.warehouse,data.destination,data.note,data.price,data.idProducer,data.idLoad);
+        res.send("Uspesan upis");
+    })
 
+    router.get ('/producer/edit',async(req,res)=>{
+        const idFreight = req.body.idFreight; // vadimo onClick
+        const freight= await getFreightId(idFreight);
+        const data={
+            weight:freight[0].weight,
+            length:freight[0].length,
+            warehouse:freight[0].warehouse,
+            destination:freight[0].destination,
+            note:freight[0].note,
+            price:freight[0].price,
+            loadType:freight[0].loadType
+        }
+        res.json({
+            weight:data.weight,
+            length:data.length,
+            warehouse:data.warehouse,
+            destination:data.destination,
+            note:data.note,
+            price:data.price,
+            loadType:data.loadType
+        });
+    });
+    router.post ('/producer/edit',async(req,res)=>{
+        const idFreight = req.body.idFreight;
+        const load = req.body.load;
+        const loadType = await getLoad();
+        const data={
+            weight:req.body.weight,
+            length:req.body.length,
+            warehouse:req.body.warehouse,
+            destination:req.body.destination,
+            note:req.body.note,
+            price:req.body.price,
+            idLoad:loadType[0].idLoad,
+            idFreight:idFreight
+        }
+        const report = await updateFreight(data.weight,data.length,data.warehouse,data.destination,data.note,data.price,data.idLoad,data.idFreight);
+        res.send("Uspesna izmena");
+    })    
+    
 
-module.exports=router;
+module.exports=router;  
